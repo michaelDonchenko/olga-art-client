cca7c50379066a49ebba5ee0bd8bc4295ff1058d
michaelDonchenko <michael1994d@gmail.com>
2021-06-26 19:48:11 +0300

    changes to home carousel

--- src/components/homepage-carousel/HomeCarousel.tsx
+++ src/components/homepage-carousel/HomeCarousel.tsx
@@ -3,11 +3,14 @@ import ImageGallery from 'react-image-gallery'
 import image1 from '../../utils/image1.jpg'
 import image2 from '../../utils/image2.jpg'
 import image3 from '../../utils/image3.jpg'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from '../../redux/store'
 
 const images = [
   {
     original: image1,
     thumbnail: image1,
+    description: 'test description',
   },
   {
     original: image2,
@@ -20,21 +23,25 @@ const images = [
 ]
 
 const HomeCarousel: React.FC = () => {
+  const {} = useSelector((state: RootState) => state.product)
+
   return (
     <Grid
       container
       style={{ margin: '5px 0', display: 'flex', justifyContent: 'center' }}
     >
       <Grid style={{ padding: '5px' }} item xs={12} sm={5}>
-        <h1 style={{ textAlign: 'center' }}>Product name</h1>
-        <p>Description</p>
+        <h1 style={{ textAlign: 'center' }}>Random Products</h1>
+        <p style={{ textAlign: 'center', color: 'gray' }}>
+          Note: Click the image to view the product
+        </p>
       </Grid>
 
       <Grid item xs={12} sm={5}>
         <ImageGallery
           showPlayButton={false}
           autoPlay={true}
-          slideInterval={3000}
+          slideInterval={5000}
           slideDuration={500}
           showThumbnails={false}
           items={images}
